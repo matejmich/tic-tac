7d88296250a1e45800cfaabec51d1db36b3c5f40
matejmich <matejmich@seznam.cz>
2024-01-07 16:52:06 +0100

    .

--- script.js
+++ script.js
@@ -7,7 +7,10 @@
 // fuck the organising.. keeps me procrastinating ○ ✗
 
 const cells = document.querySelectorAll(".cell")
+const restart = document.getElementById("restart")
+restart.addEventListener("click", restartGame)
 let currentPlayer = "✗"
+const winText = document.getElementById("textWin")
 const winCondition = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,7 +21,7 @@ const winCondition = [
     [1, 4, 7],
     [2, 5, 8]
 ]
-options = ["", "", "", "", "", "", "", "", ""]
+let options = ["", "", "", "", "", "", "", "", ""]
 
 startGame()
 function startGame() {
@@ -52,6 +55,7 @@ function changePlayer() {
 }
 
 function checkWin() {
+    let win = false
     for (let  i = 0; i < winCondition.length; i++ ) {
     const condition = winCondition[i]
     const cellA = options[condition[0]]
@@ -65,7 +69,16 @@ function checkWin() {
         console.log("won")
         console.log(condition[0], condition[1], condition[2])
         highlightCells(condition[0], condition[1], condition[2])
+        winText.textContent = "That's a win!"
+        win = true
+        break;
     }
+   
+    }
+    if (win === false) {
+        if (!options.includes("")) {
+            winText.textContent = "That's a draw!"
+        }
     }
 }
 
@@ -76,4 +89,10 @@ function highlightCells(first, second, third) {
     cellOne.classList.add("won")
     cellTwo.classList.add("won")
     cellThree.classList.add("won")
+}
+function restartGame() {
+ cells.forEach(cell => cell.textContent = "")
+ cells.forEach(cell => cell.classList.remove("won"))
+ options = ["", "", "", "", "", "", "", "", ""]
+ winText.textContent = "Tic Tac Toe! :)"
 }
